f047c8f173dc0abfc39d060fd3f82bc223aabd46
Anjana Sarath <anjanasarath5@gmail.com>
2018-04-11 15:46:46 +0530

    style correction

--- app/src/js/components/findParking.js
+++ app/src/js/components/findParking.js
@@ -8,8 +8,6 @@ import AutoCompleteComponent from './auto_complete';
 const style1 = {
   buttonStyle : {
     backgroundColor:"#0a0",
-    width: '100%',
-    height: '100%'
     }
   }
 
@@ -18,16 +16,22 @@ export default class FindParking extends React.Component {
     return (
         <div>
           <div id="parking" className="containerDiv">
+            <div id="subpark" className="parkingInnerDiv">
             <h1 className="h1">Find parking in <span className="secondsFont">seconds</span></h1>
-            <h4 className="h1">Choose from millions of available spaces, or reserve your space
+            <h4 className="h4">Choose from millions of available spaces, or reserve your space
              in advance. Join over 1.5 million drivers enjoying easy parking.</h4>
-          <div id="autocmplte2" className="inputMainUi1">
-            <AutoCompleteComponent/>
-          </div>
-          <div className="Rbtn">
-            <RaisedButton buttonStyle={style1.buttonStyle}>
-             search
-            </RaisedButton>
+           <div id="autocmplte2" className="center auto1">
+              <AutoCompleteComponent/>
+             </div>
+            <div id="autocmplte2" className="row auto2">
+              <AutoCompleteComponent/>
+              <AutoCompleteComponent/>
+            </div>
+            <div id="rbtn" className="Rbtn auto2">
+              <RaisedButton buttonStyle={style1.buttonStyle}>
+                search
+              </RaisedButton>
+            </div>
           </div>
           </div>
         </div>
---
+++ app/src/js/components/multipleLogin.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
+
+export default class MultipleLoginComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          hi
+        </div>
+      </div>
+    );
+  }
+}
